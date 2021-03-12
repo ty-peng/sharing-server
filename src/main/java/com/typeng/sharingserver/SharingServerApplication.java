c8e862bccaf5ef8bba095812cd6b298a47ece107
ty-peng <tiany.dev@qq.com>
2021-03-12 13:59:41 +0800

    feat: Initialize the project

---
+++ src/main/java/com/typeng/sharingserver/SharingServerApplication.java
@@ -0,0 +1,13 @@
+package com.typeng.sharingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/typeng/sharingserver/SharingServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.typeng.sharingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingServerApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
