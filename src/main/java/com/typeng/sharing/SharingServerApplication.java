226a62a31f5d1293b5db67caa695290c10b5c62a
ty-peng <tiany.dev@qq.com>
2021-03-16 19:08:08 +0800

    refactor: rename package

--- src/main/java/com/typeng/sharing/SharingServerApplication.java
+++ src/main/java/com/typeng/sharing/SharingServerApplication.java
@@ -1,4 +1,4 @@
-package com.typeng.sharingserver;
+package com.typeng.sharing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/typeng/sharing/SharingServerApplicationTests.java
+++ src/test/java/com/typeng/sharing/SharingServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.typeng.sharingserver;
+package com.typeng.sharing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
